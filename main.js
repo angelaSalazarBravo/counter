d70c0def10d4efd19cc4bf31af92543519a0de3b
angelaSalazarBravo <angie.skyw@gmail.com>
2023-10-18 13:19:28 +0200

    counter

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const button_decr = document.getElementsByClassName('btn')[0];
+const numberElement = document.querySelector('h1.number');
+let count = 0; 
+button_decr.addEventListener('click', function() {
+  
+  count -= 1;
+  numberElement.textContent = count;
+});
+
+const button_reset = document.getElementsByClassName('btn')[1];
+button_reset.addEventListener('click', function() {
+  
+  count = 0;
+  numberElement.textContent = count;
+});
+
+const button_sum = document.getElementsByClassName('btn')[2];
+button_sum.addEventListener('click', function(){
+
+  count += 1;
+  numberElement.textContent = count;
+});
